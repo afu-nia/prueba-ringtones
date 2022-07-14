27e274e17c931bb1aba0d9d894fd5f47ceb7a6d9
afu-nia <109266685+afu-nia@users.noreply.github.com>
2022-07-14 02:24:54 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -6,22 +6,24 @@ function chillHop() {
         {
             name:"andamos guerriando",
             artist: "Arcangel",
-            cover: "https://cdn.dribbble.com/users/1237300/screenshots/6478927/__-1_1_____.gif",
+            cover: "https://static.vecteezy.com/system/resources/previews/001/792/471/mp4/music-equalizer-for-music-party-free-video.mp4",
             id: uuidv4(),
             color: ["#F24933", "#F9B437"],
             audio: "https://doc-08-ao-docs.googleusercontent.com/docs/securesc/2an6hacm01c84md773cjc90sio258f8o/2r30s4evtf1n5ll9qrtis1c6mm8vq4dh/1657782975000/01897180926544070735/01897180926544070735/1IFOXXnaokseD-Wjh8oqY4rIXimejhy1f?e=download&ax=ACxEAsYQjXdyMo1Dpy2_DdL1aWGb1mWixgxBwNcIFRXke5dUm7dzfNFu5Mk6D2G82RnyMKAp7BZREMSlEMX5dqoyvx7f99GwMK2o__PPAmt1lSm3R25i9mUTdiHKsDTftuUOObkQMW7CJLXZAlF7OA2CmuMOcmGCOvkxnPn8BgN7-77yHrs0AFTs1RVbSE_67ZivVsyMVb6jXiCRPENzwsX34pucrxdfwPb4YpYMAb8mGkUe3DbCXMVZiU5GIBYmXAH7NKfNTh5LGys7dPPmoetBfJly9aySzWqMsNzElMQkKOetqPTRttbYvzc7OWzRZ0l17v_kVICkMwTKJQRARUDtTgWAL4MvdEPD5WaP0op4BTeJueIyfXn3fekjbv-YQSJMEBCwa4ZRkPe8a3ggjumOJyssrdIqe6JQ_OVSJ_GNiBr4taJFnOm7ftWZ8GGyKELaB4Szkc5MFfeykLnbN5YSTnUg8Wrl7NOvhZhWZ1tUp2xrGFLD4avVIHf5mJKbe79E1lWv5j_7iJ4nuUjPWDZVwQcrXySXdp16UrnJTLP2HUO-JEh83XmxMxvddAKhoE7LTnDP9jIfVZ775QlvfnODDvAHxC4B0YLb5r4OQpxm19ADgry79aZUp_Q6KzpMy1cihxOYo5DlQZiRJCI5mQp1IG47VmgNBYLZYFsG571nxWP005xJ5un5vd9Sdf2eS8zI4haowkem5o81b6GG&uuid=5a1aa185-fede-44f4-b06e-52cef6b2b01a&authuser=0",
             active: true,
         },
         {
-            name:"prueba 2",
+            name:"Telugu 2",
             artist: "Lounge",
-            cover: "https://png.pngtree.com/png-vector/20190726/ourlarge/pngtree-modern-square-cool-music-for-party-lights-png-image_1629696.jpg",
+            cover: "https://static.vecteezy.com/system/resources/previews/001/792/471/mp4/music-equalizer-for-music-party-free-video.mp4",
             id: uuidv4(),
             color: ["#D7B0D5", "#CBDFE0"],
             audio: "https://doc-04-ao-docs.googleusercontent.com/docs/securesc/2an6hacm01c84md773cjc90sio258f8o/4jsatimgshipmvugi7k3kr9g86sb91fk/1657783200000/01897180926544070735/01897180926544070735/1zd0pHTTj5swv0WV22cqgq1HEfwTEwXKo?e=download&ax=ACxEAsZiODvfFq66VRcrBB-7b_tjuQtQ3fuiEbMPLbKTFY3zlB0y5afmd-tzq2ptLLu2vaxyRi5xntMTm2o8wd6pJK5W2k96ybYKqXuuyYUjM53HQLRQyClJ8mchnV1rhsHazBUgnS9SNE5zTZAXJjkSrDNFzHnZVzXwTD0Wzdu9KMQWedvLx7omCYmcqJ924SKguyeGi8NgtNPapEDG1bIkepsCOVjIV3XZvOhi-MfVDs4KoIOAc_saKjCkCIkd9Ye2zU_X6g8XqUOXIVWGc_zqpN8H5VXcCpwoNCmdT7DgNm-0kZm7AupL68dlx1b5w7pnAMd9jKxOcwAaCF0Wb07dOUEcXX7DK_XlQnnMviAgJ6cGsp5iKHYk3jZs5JRyspAe-wkVUqFtLNchZ2ECLnkYdZQo0QH6DfctwkItgfiNEi_aphrCXHFMJ89_TG5L33Ps3_DRa7uTvkFrDwLduCEioPnAq66mWJko8cpA8CGI6qDP5Nh98BLggupvxYaET16NEulegO4Kz-hOdB2e7nYXvh0HBBPEJ18ibmm6gENz6oP2FdfFpW35Hr3eIrWZ75TO__mIX0j3_NNRspSvawsGDPywjRA3EzB_hF-nHcHVjRewJHFIPkIC8mKVuMgqu8P_GF7Gno-luWi2ujdx2xMhr2fuIxumsuuVxggcgmd3vxLv4KaJJS4WHHMQsKJ0npAaCWmmR8oBdscIfc45&uuid=a4e83b5e-787a-4970-a934-b2d42af33b45&authuser=0&nonce=7qdod819u17og&user=01897180926544070735&hash=gs1oh4djfdah2k8pruskgg9qrdjiv97c",
             active: false,
         },
-  
+      
+    ];
+}
 
 export default chillHop;
 
